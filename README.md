9c93ec96c146287cd6f4b254deebaa76361d132e
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-03-20 11:42:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# kintone-disguise-my-profile 
+## Description
+kintoneのプロフィール画像を自分のお気に入りの画像にすり替えるChrome拡張。
+
+自分以外のユーザーには共通管理画面で設定したプロフィール画像が表示されます。
+
+## License
+Copyright (c) 2020 kuroppe1819
+
+Licensed under the <a href="LICENSE">MIT</a> license.
---
+++ webpack/webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = '../src/';
+
+module.exports = {
+    entry: {
+        content_script: path.join(__dirname, srcDir + 'content_script.ts')
+    },
+    output: {
+        path: path.join(__dirname, '../dist/js'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    plugins: [
+        // exclude locale files in moment
+        new CopyPlugin([
+            { from: '.', to: '../' }
+          ],
+          {context: 'public' }
+        ),
+    ]
+};
