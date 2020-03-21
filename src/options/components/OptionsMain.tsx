9383d0d0611288898faead8cedf99b49aa033536
kuroppe <kuroppems@gmail.com>
2020-03-21 13:49:02 +0900

    オプション画面用のscssクラスを作成

--- src/options/components/OptionsMain.tsx
+++ src/options/components/OptionsMain.tsx
@@ -3,11 +3,11 @@ import { DropArea } from './DropArea';
 
 export const OptionsMain = () => {
     return (
-        <div className="option-main">
+        <div className="options-main">
             <DropArea />
-            <div className="option-main__user-select">
-                <button className="option-main__user-select-upload">UPLOAD</button>
-                <button className="option-main__user-select-delete">DELETE</button>
+            <div className="options-main__user-select">
+                <button className="options-main__user-select-upload">UPLOAD</button>
+                <button className="options-main__user-select-delete">DELETE</button>
             </div>
         </div>
     );
