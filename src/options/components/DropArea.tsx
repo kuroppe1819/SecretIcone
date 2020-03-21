fce7d24ed9315ae15946d9eb875d8b373f363dba
kuroppe <kuroppems@gmail.com>
2020-03-21 16:14:23 +0900

    オプション画面のスタイルを作成

--- src/options/components/DropArea.tsx
+++ src/options/components/DropArea.tsx
@@ -2,20 +2,20 @@ import * as React from 'react';
 import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 
-export const DropArea = () => {
+type Props = {
+    className: string;
+};
+
+export const DropArea = (props: Props) => {
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
     }, []);
     const { getRootProps, getInputProps, isDragActive } = useDropzone(onDrop);
 
     return (
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {isDragActive ? (
-                <p>Drop the files here ...</p>
-            ) : (
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            )}
+        <div className={`drop-area ${props.className}`} {...getRootProps()}>
+            <input className="drop-area__input" {...getInputProps()} />
+            {isDragActive ? <p>ドラッグ中</p> : <p>ここにファイルをドロップしてくれ</p>}
         </div>
     );
 };
--- src/options/components/OptionsMain.tsx
+++ src/options/components/OptionsMain.tsx
@@ -4,7 +4,7 @@ import { DropArea } from './DropArea';
 export const OptionsMain = () => {
     return (
         <div className="options-main">
-            <DropArea />
+            <DropArea className={'options-main-drop-area'} />
             <div className="options-main__user-select">
                 <button className="options-main__user-select-upload">UPLOAD</button>
                 <button className="options-main__user-select-delete">DELETE</button>
