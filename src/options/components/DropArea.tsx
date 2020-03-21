acf5a69a473d1f348881c56bf79e5d75bb84317d
kuroppe <kuroppems@gmail.com>
2020-03-21 19:49:25 +0900

    ドラッグ＆ドロップエリアのデザインを修正

--- src/options/components/DropArea.tsx
+++ src/options/components/DropArea.tsx
@@ -9,13 +9,33 @@ type Props = {
 export const DropArea = (props: Props) => {
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
+        console.log(acceptedFiles);
     }, []);
-    const { getRootProps, getInputProps, isDragActive } = useDropzone(onDrop);
+    const onDropObj: any = { onDrop: onDrop };
+    const { getRootProps, getInputProps, isDragActive } = useDropzone(onDropObj);
 
     return (
         <div className={`drop-area ${props.className}`} {...getRootProps()}>
             <input className="drop-area__input" {...getInputProps()} />
-            {isDragActive ? <p>ドラッグ中</p> : <p>ここにファイルをドロップしてくれ</p>}
+            {isDragActive ? (
+                <div className="drop-area__input__inside">
+                    <div className="drop-area__input__inside__sample-images">
+                        <div className="drop-area__input__inside__sample-images__jpg"></div>
+                        <div className="drop-area__input__inside__sample-images__png"></div>
+                        <div className="drop-area__input__inside__sample-images__gif"></div>
+                    </div>
+                    <p className="drop-area__input__inside__text">ここに画像をドロップします</p>
+                </div>
+            ) : (
+                <div className="drop-area__input__inside drop-area__input__inside--non-active">
+                    <div className="drop-area__input__inside__sample-images">
+                        <div className="drop-area__input__inside__sample-images__jpg"></div>
+                        <div className="drop-area__input__inside__sample-images__png"></div>
+                        <div className="drop-area__input__inside__sample-images__gif"></div>
+                    </div>
+                    <p className="drop-area__input__inside__text">ここに画像をドラッグします</p>
+                </div>
+            )}
         </div>
     );
 };
