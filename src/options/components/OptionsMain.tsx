30aa72f34faca7d340792d7a0c013c80d7425aa1
kuroppe <kuroppems@gmail.com>
2020-03-21 11:33:44 +0900

    UPLOAD, DELETEボタンにスタイルを当てる

--- src/options/components/OptionsMain.tsx
+++ src/options/components/OptionsMain.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
+import { DropArea } from './DropArea';
 
 export const OptionsMain = () => {
-    return <button className="hoge">button</button>;
+    return (
+        <div className="option-main">
+            <DropArea />
+            <div className="option-main__user-select">
+                <button className="option-main__user-select-upload">UPLOAD</button>
+                <button className="option-main__user-select-delete">DELETE</button>
+            </div>
+        </div>
+    );
 };
